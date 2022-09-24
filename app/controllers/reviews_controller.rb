04ada3fbf0c5ab2553397351fc590b668e914f4c
nzila ikouara martsia monange <nzilax@gmail.com>
2022-09-24 02:11:03 +0200

    modification

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,9 @@
 class ReviewsController < ApplicationController
 
-  def index
-    @reviews = Review.all
-    #méthode à revoir
-  end
+  # def index
+  #   @reviews = Review.all
+  #   #méthode à revoir
+  # end
 
   def new
     @review = Review.new
@@ -12,8 +12,8 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.booking = Booking.find(params[:booking_id])
-    @review.user = current_user
-    if @review.save
+    #@review.user = current_user
+    if @review.save?
       redirect_to booking_path(@review.booking)
     else
       render :new
@@ -26,7 +26,6 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:rate_as_client, :about)
     # Ligne de code à revoir car il manque le rate_as_offer
   end
-
 end
 
 # D'après nos routes on devrait avoir aussi show mais j'ai un doute sur l'importance de cette méthode
