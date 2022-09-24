db28045450ca9ea250513183c55b01bf0aa2aa55
nzila ikouara martsia monange <nzilax@gmail.com>
2022-09-24 12:24:05 +0200

    inversion de la foreign key entre users et bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_193315) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_102015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_193315) do
     t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
     t.index ["article_id"], name: "index_bookings_on_article_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -63,6 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_193315) do
   end
 
   add_foreign_key "bookings", "articles"
+  add_foreign_key "bookings", "users", column: "users_id"
   add_foreign_key "reviews", "bookings"
-  add_foreign_key "users", "bookings", column: "bookings_id"
 end
