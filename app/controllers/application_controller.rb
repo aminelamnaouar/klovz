238cd297c49774a94b15c717aea3f3d8ecb8934a
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-09-28 00:47:18 +0200

    booking fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+# To enable sign in to function correctly.
+before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :authenticate_user!
+def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation, :roles])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :email, :password, :remember_me])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation])
+    end
 end
