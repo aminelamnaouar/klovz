a3aa7713a30cc76e2de3d54250b932fcfbd48a35
nzila ikouara martsia monange <nzilax@gmail.com>
2022-10-01 01:51:40 +0200

    ajout du champ image sur articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_001204) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_233230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_001204) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.string "image"
   end
 
   create_table "bookings", force: :cascade do |t|
